eb24b687f8c691824160ea252813b7a1e9ab0c0b
Chase Sang <chase.sang@gmail.com>
2017-06-15 21:22:10 -0700

    add bookmarks

--- app/controllers/mybookmarks_controller.rb
+++ app/controllers/mybookmarks_controller.rb
@@ -6,4 +6,23 @@ def index
   @bookmarks = Bookmark.all
 end
 
+def new
+  @bookmark = @user.bookmarks.new 
+end
+
+def create
+  @bookmark = Bookmark.new bookmark_params
+  if @bookmark.save
+    redirect_to user_mybookmarks_path
+  else
+    redirect_to user_mybookmarks_path
+  end
+end
+
+private
+
+def bookmark_params
+  params.require(:bookmark).permit([:content, :user_id])
+end
+
 end
