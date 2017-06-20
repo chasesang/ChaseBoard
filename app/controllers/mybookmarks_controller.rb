c663c486c38162ee0a4a37d2e39e31799330dd0b
Chase Sang <chase.sang@gmail.com>
2017-06-19 20:36:55 -0700

    something new2

--- app/controllers/mybookmarks_controller.rb
+++ app/controllers/mybookmarks_controller.rb
@@ -1,28 +1,28 @@
 class MybookmarksController < ApplicationController
 
-# before_action :authenticate_user!
-#
-# def index
-#   @bookmarks = Bookmark.all
-# end
-#
-# def new
-#   @bookmark = @user.bookmarks.new
-# end
-#
-# def create
-#   @bookmark = Bookmark.new bookmark_params
-#   if @bookmark.save
-#     redirect_to user_mybookmarks_path
-#   else
-#     redirect_to user_mybookmarks_path
-#   end
-# end
-#
-# private
-#
-# def bookmark_params
-#   params.require(:bookmark).permit([:content, :user_id])
-# end
+before_action :authenticate_user!
+
+def index
+  @bookmarks = Bookmark.all
+end
+
+def new
+  @bookmark = Bookmark.new
+end
+
+def create
+  @bookmark = Bookmark.new bookmark_params
+  if @bookmark.save
+    redirect_to user_mybookmarks_path
+  else
+    redirect_to user_mybookmarks_path
+  end
+end
+
+private
+
+def bookmark_params
+  params.require(:bookmark).permit([:content, :user_id])
+end
 
 end
